366656e73339eae6b5382ce9af768b2e07bfe340
Bhavya Surana <surana.bhavya01@gmail.com>
2026-05-01 12:20:52 +0530

    Adding swagger dependencies

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
